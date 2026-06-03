3ab90a2a416c70fc26ee70a868e219d32d762c20
Socialtellers <info@socialtellers.co>
2026-06-03 12:06:37 +0400

    fix: template language en_US + sidebar color fix

--- server/index.js
+++ server/index.js
@@ -714,7 +714,7 @@ async function sendWhatsAppTemplate(to) {
         type: 'template',
         template: {
           name: 'social_tellers_outreach',
-          language: { code: 'en' }
+          language: { code: 'en_US' }
         }
       })
     }
